a3b0685ffb430dd59607a654d6e5c31118e5b59a
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-07-13 18:04:41 +0500

    Update methods in post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,13 @@ class Post < ApplicationRecord
     has_many :commentss, foreign_key: 'post_id'
     belongs_to :author, class_name: 'User'
 
+    after_save :update_posts_counter
+
     def five_recent_comments
         comments.order(created_at: :desc).limit(5)
       end
+    
+    private
 
     def update_posts_counter
         author.increment!(:posts_counter)

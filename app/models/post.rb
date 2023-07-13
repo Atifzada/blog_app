c72524b3c1651914235d874a9bf7936d18e7625b
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-07-13 21:36:47 +0500

    Add rspec for user and its methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,18 @@
 class Post < ApplicationRecord
-  has_many :posts, foreign_key: 'post_id'
-  has_many :commentss, foreign_key: 'post_id'
   belongs_to :author, class_name: 'User'
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
+  
 
   after_save :update_posts_counter
 
   def five_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
+  
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   private
 
@@ -15,8 +20,4 @@ class Post < ApplicationRecord
     author.increment!(:posts_counter)
   end
 
-  validates : title, presence: true
-  validates :title,length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  subject { User.create(name: 'Atif', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'A Pakistani Micronauts.', posts_counter: 0) }
+
+  before { subject.save }
+
+  it 'user successful created' do
+    expect(subject).to be_valid
+  end
+
+  it 'name cannot be black' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it 'posts_counter cannot be blank' do
+    subject.posts_counter = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'name cannot be empty' do
+    subject.name = ''
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts_counter must be integer' do
+    subject.posts_counter = 1.2
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts_counter should be greater or equal to 0' do
+    subject.posts_counter = 0
+    expect(subject).to be_valid
+  end
+
+  it 'posts_counter should be greater or equal to 0' do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts_counter be bigger or equal to 0' do
+    subject.posts_counter = 200
+    expect(subject).to be_valid
+  end
+
+end

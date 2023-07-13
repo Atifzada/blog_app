d8fb44b6964860274cbf352f01a0b2c2c5366e8b
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-07-13 18:06:34 +0500

    Update methods in Comment class

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,11 @@ class Comment < ApplicationRecord
     belongs_to :author, class_name: 'User'
     belongs_to :post
     
-  after_save :updte_comment_counter
+  after_save :update_comments_counter
 
   private
 
-  def updte_comment_counter
+  def update_comments_counter
     post.increment!(:comments_counter)
   end
 end

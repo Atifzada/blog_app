f320f281109f574afb7460558fed204be38a7a99
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-07-13 17:48:53 +0500

    Add recent_posts and update_post_counter methods to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
     has_many :comments, foreign_key: 'author_id'
     has_many :comments, foreign_key: 'author_id'
     
-    scope :three_most_recent_posts, ->(user) { user.posts.order(created_at: :desc).limit(3) }
+    def three_most_recent_posts
+        posts.order(created_at: :desc).limit(3)
+      end
 end

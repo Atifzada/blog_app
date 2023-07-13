1b40a115bb570a2e3ec58316815b8649739a984d
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-07-13 20:11:56 +0500

    Add validation to Post class as per the requirments

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,4 +14,9 @@ class Post < ApplicationRecord
   def update_posts_counter
     author.increment!(:posts_counter)
   end
+
+  validates : title, presence: true
+  validates :title,length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 end

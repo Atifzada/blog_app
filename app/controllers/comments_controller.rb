e2bab4a386bb30db60b70743b1e77b916ccdcc71
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-07-27 23:16:39 +0500

    Make changes as per the reviewer request

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
   def new
     @comment = Comment.new
   end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,15 +2,21 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, Post, Public: true
-    can :read, Comment, Public: true
+    can :read, Post, public: true
+    can :read, Comment, public: true
 
+    # user can read their own posts
     return unless user.present?
 
-    can :read, :all
+    can :read, Post, user_id: user.id
+    can :destroy, Post, author_id: user.id
 
-    return unless user.role == 'admin'
+    can :read, Comment, user_id: user.id
+    can :destroy, Comment, author_id: user.id
 
-    can :manage, :all
+    return unless user.role == 'admin' # permission for admins
+
+    can :destroy, Post
+    can :destroy, Comment
   end
 end
